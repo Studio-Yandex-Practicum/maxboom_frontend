0293bc7471dba8df6c3abd526a725a2f910d025b
Ilphat <90051828+Ilph@users.noreply.github.com>
2023-08-28 19:25:10 +0300

    `#1-initial-settings-of-project` (#2)
    Создал базовые настройки проекта. Подключены: React, typescript, eslint, stylelint, prettier, lefthook, webpack.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from './components/test/test'
+
+function App() {
+  return (
+    <>
+      <Test />
+      <div>Hello, world</div>
+    </>
+  )
+}
+
+export default App
