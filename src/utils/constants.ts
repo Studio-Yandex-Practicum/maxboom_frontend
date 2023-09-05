46814a0ba8e32361fec75b75f70d4b3a8bc9b7de
Marina Gutkevich <gutmalina@mail.ru>
2023-09-05 16:47:01 +0300

    #7-add-style-variables

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,54 +1,6 @@
-import ImgStories01 from '../images/stories/img-stories-01.svg'
-import ImgStories02 from '../images/stories/img-stories-02.svg'
-import ImgStories03 from '../images/stories/img-stories-03.svg'
-import ImgStories04 from '../images/stories/img-stories-04.svg'
-import ImgStories05 from '../images/stories/img-stories-05.svg'
-import ImgStories06 from '../images/stories/img-stories-06.svg'
-import ImgStories07 from '../images/stories/img-stories-07.svg'
-import ImgStories08 from '../images/stories/img-stories-08.svg'
-
-export const TEXT_STORIES = 'Истории'
-export const ALT_IMG_STORIES = 'Изображение акции'
-
-export const ARR_STORIES = [
-  {
-    id: 1,
-    src: ImgStories01
-  },
-  {
-    id: 2,
-    src: ImgStories02
-  },
-  {
-    id: 3,
-    src: ImgStories03
-  },
-  {
-    id: 4,
-    src: ImgStories04
-  },
-  {
-    id: 5,
-    src: ImgStories05
-  },
-  {
-    id: 6,
-    src: ImgStories06
-  },
-  {
-    id: 7,
-    src: ImgStories07
-  },
-  {
-    id: 8,
-    src: ImgStories08
-  },
-  {
-    id: 9,
-    src: ImgStories02
-  },
-  {
-    id: 10,
-    src: ImgStories05
-  }
-]
+export const TEXT_STORIES: string = 'Истории'
+export const TEXT_BLOG: string = 'Блог'
+export const TEXT_NEWS: string = 'Новости'
+export const TEXT_POPULAR_CATEGORIES: string = 'Популярные категории'
+export const TEXT_CUSTOMERS_ABOUT_US: string = 'Наши клиленты о нас'
+export const TEXT_OUR_BRANDS: string = 'Наши бренды'
