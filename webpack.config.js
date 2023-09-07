2196386d64e592881d6ebb4828e5fd6dd5993616
Julia Avramenko <miraclejuliet@yandex.ru>
2023-09-07 14:50:54 +0300

    #10 Add Logo component

--- webpack.config.js
+++ webpack.config.js
@@ -30,19 +30,19 @@ const config = {
         },
         isEnvProduction
           ? {
-              minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true
-              }
+            minify: {
+              removeComments: true,
+              collapseWhitespace: true,
+              removeRedundantAttributes: true,
+              useShortDoctype: true,
+              removeEmptyAttributes: true,
+              removeStyleLinkTypeAttributes: true,
+              keepClosingSlash: true,
+              minifyJS: true,
+              minifyCSS: true,
+              minifyURLs: true
             }
+          }
           : undefined
       )
     ),
