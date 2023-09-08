cbd14045a523da9080407b442d66c1c21d8ad07a
Ilphat <ilphat.gi@gmail.com>
2023-09-08 19:12:37 +0300

    #4-make-style-settings add normalise.scss, refactoring styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import Slider from './components/Slider/Slider'
+import Stories from './components/stories/stories'
 
 function App() {
-  return <Slider />
+  return (
+    <>
+      <Slider />
+      <Stories />
+    </>
+  )
 }
 
 export default App
