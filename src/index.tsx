0c02105da4f9976d7bdd968c42eabd14c592bf5f
Ilphat <90051828+Ilph@users.noreply.github.com>
2023-09-08 10:35:04 +0300

    #8-add-redux-toolki (#11)

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.scss'
+import { Provider } from 'react-redux'
+import store from './store/index'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
