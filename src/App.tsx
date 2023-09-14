ce41d59807c75ea4722b95a66e5841790be8388f
Максим Луговской <lugovskou.myu@yandex.ru>
2023-09-14 21:18:59 +0600

    #15-create-component-subscribe

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Slider from './components/Slider/Slider'
-import Stories from './components/Stories/Stories'
+import Stories from './components/stories/stories'
+import { Subscribe } from './components'
 
 function App() {
   return (
     <>
       <Slider />
       <Stories />
+      <Subscribe />
     </>
   )
 }
