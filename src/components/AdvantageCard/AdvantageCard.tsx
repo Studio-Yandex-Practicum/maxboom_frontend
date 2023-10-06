56b2f67a2cc6c4778164e3d333abf56414557159
Julia Avramenko <miraclejuliet@yandex.ru>
2023-10-06 14:59:35 +0300

    #20-bug-fix

--- src/components/AdvantageCard/AdvantageCard.tsx
+++ src/components/AdvantageCard/AdvantageCard.tsx
@@ -1,24 +1,24 @@
-import React, { FC, ReactNode } from 'react'
+import React, { FC, FunctionComponent, SVGProps } from 'react'
 import styles from './advantageCard.module.scss'
 
-type TAdvantageCardProps = {
-  src: string
-  readonly children: ReactNode
+export type TAdvantageCardProps = {
+  image: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string | undefined }> & '*.svg'
+  alt: string
+  name: string
 }
-
 /**
- * @param {string} src - картинка, которая вставляется в определенную карточку
+ * @param {string} image - картинка для преимущства
+ * @param {string} alt - описание картинки
+ * @param {string} name - название преимущества
  */
-
-const AdvantageCard: FC<TAdvantageCardProps> = ({ children, src }) => {
+const AdvantageCard: FC<TAdvantageCardProps> = ({ image, alt, name }) => {
   return (
     <div className={`${styles.card}`}>
       <a href="#" className={`${styles.link}`}>
-        <img src={src} alt="img" className={`${styles.image}`} />
-        <p className={`${styles.text}`}>{children}</p>
+        <img src={image} alt={alt} className={`${styles.image}`} />
+        <p className={`${styles.text}`}>{name}</p>
       </a>
     </div>
   )
 }
-
 export default AdvantageCard
