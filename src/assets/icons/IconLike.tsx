ad89237ccd5dc8f1814d263d32341d7e4f54019f
Kirill Kurentsov <immortalsteel@gmail.com>
2023-10-22 13:41:29 +0300

    #40-product-card-component

---
+++ src/assets/icons/IconLike.tsx
@@ -0,0 +1,40 @@
+import React, { FC, useMemo } from 'react'
+import { TProps } from '../../models/SvgModel'
+
+/**
+ * @param {Array<string> | string} styles - стилизация иконки
+ *
+ * @return {svg} - svg изображение
+ */
+const IconLike: FC<TProps> = props => {
+  const { styles } = props
+  const style = useMemo(() => {
+    return Array.isArray(styles) ? styles.join(' ') : styles
+  }, [styles])
+
+  return (
+    <svg viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg" className={style}>
+      <g clipPath="url(#clip0_110_50)">
+        <g clipPath="url(#clip1_110_50)">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M16.6579 2.8558C15.7632 1.87159 14.6 1.33475 13.2579 1.33475C12.2737 1.33475 11.3789 1.60317 10.5737 2.22948C10.2158 2.49791 9.76842 2.94527 9.5 3.39264C9.14211 2.94527 8.78421 2.58738 8.42632 2.22948C7.62105 1.60317 6.72632 1.33475 5.74211 1.33475C4.4 1.33475 3.23684 1.87159 2.34211 2.8558C1.44737 3.75054 1 5.09264 1 6.43475C1 7.86633 1.53684 9.20843 2.7 10.64C3.77368 11.8926 5.20526 13.1453 6.90526 14.6663C7.53158 15.1137 8.15789 15.6505 8.87368 16.2769C9.05263 16.4558 9.32105 16.5453 9.5 16.5453C9.76842 16.5453 9.94737 16.4558 10.1263 16.2769C10.8421 15.6505 11.4684 15.1137 12.0947 14.5769C13.7947 13.1453 15.3158 11.8032 16.3 10.5505C17.4632 9.11896 18 7.86633 18 6.34527C18 5.09264 17.5526 3.75054 16.6579 2.8558Z"
+            stroke="currentColor"
+            strokeWidth="1.78947"
+          />
+        </g>
+      </g>
+      <defs>
+        <clipPath id="clip0_110_50">
+          <rect width="19" height="17" fill="currentColor" transform="translate(0 0.440002)" />
+        </clipPath>
+        <clipPath id="clip1_110_50">
+          <rect width="19" height="17" fill="currentColor" transform="translate(0 0.440002)" />
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
+
+export default IconLike
--- src/pages/ProductsPage/ProductsPage.tsx
+++ src/pages/ProductsPage/ProductsPage.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from 'react'
-import Header from '../../components/header/header'
-import Footer from '../../components/Footer/Footer'
 import WrapperForMainContent from '../../components/WrapperForMainContent/WrapperForMainContent'
 import { CategoryList } from '../../components/CategoryList/CategoryList'
 import { PageDescription } from '../../components/PageDescription/PageDescription'
 import { PageControls } from '../../components/PageControls/PageControls'
 import { Pagination } from '../../components/Pagination/Pagination'
+import { ProductCard } from '../../components/ProductCard/ProductCard'
 import { ITEMS_PER_PAGE_OPTION, SORT_OPTION, TOTAL_PAGES } from '../../mockData/productsPageOptions'
 import { ECardView } from '../../utils/types'
 import styles from './ProductsPage.module.scss'
@@ -52,35 +51,36 @@ export const ProductsPage = () => {
   }
 
   return (
-    <>
-      <Header />
-      <main className={styles.main}>
-        <WrapperForMainContent>
-          <div className={styles.content}>
-            <PageDescription />
-            <div className={styles['content-grid']}>
-              <CategoryList />
-              <div className={styles['content-main']}>
-                <PageControls
-                  cardView={cardView}
-                  handleCardViewChange={handleCardViewChange}
-                  handleItemsPerPageChange={handleItemsPerPageChange}
-                  handleSortChange={handleSortChange}
-                  itemPerPageOptions={ITEMS_PER_PAGE_OPTION}
-                  sortOptions={SORT_OPTION}
-                />
-                <Pagination
-                  currentPage={currentPage}
-                  totalPages={TOTAL_PAGES}
-                  handlePageChange={handlePageChange}
-                  handleShowMore={handleShowMore}
-                />
-              </div>
+    <main className={styles.main}>
+      <WrapperForMainContent>
+        <div className={styles.content}>
+          <PageDescription />
+          <div className={styles['content-grid']}>
+            <CategoryList />
+            <div className={styles['content-main']}>
+              <PageControls
+                cardView={cardView}
+                handleCardViewChange={handleCardViewChange}
+                handleItemsPerPageChange={handleItemsPerPageChange}
+                handleSortChange={handleSortChange}
+                itemPerPageOptions={ITEMS_PER_PAGE_OPTION}
+                sortOptions={SORT_OPTION}
+              />
+              <section className={styles['content-products']}>
+                {[0, 1, 2, 3, 4, 5, 6, 7].map(x => (
+                  <ProductCard key={x} layout={cardView} />
+                ))}
+              </section>
+              <Pagination
+                currentPage={currentPage}
+                totalPages={TOTAL_PAGES}
+                handlePageChange={handlePageChange}
+                handleShowMore={handleShowMore}
+              />
             </div>
           </div>
-        </WrapperForMainContent>
-      </main>
-      <Footer />
-    </>
+        </div>
+      </WrapperForMainContent>
+    </main>
   )
 }
