7fb694a2a5d8cd168dec003811e59914e50fe76b
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-11-20 09:03:39 +0300

    pagination-blog-fixed

--- src/components/BlogMain/BlogMain.tsx
+++ src/components/BlogMain/BlogMain.tsx
@@ -8,15 +8,13 @@ import BlogCategories from '../BlogCategories/BlogCategories'
 import WrapperForMainContent from '../WrapperForMainContent/WrapperForMainContent'
 import BlogMainItem from '../BlogMainItem/BlogMainItem'
 import { Pagination } from '../Pagination/Pagination'
-import Heading from '../../shared/ui/Heading/Heading'
-import Subheading from '../../shared/ui/Subheading/Subheading'
 
 const BlogMain: FC<PropsBlog> = props => {
   const { cards } = props
   const [items, setItems] = useState(cards)
   const [itemNumber, setItemNumber] = useState(9)
   const [currentPage, setCurrentPage] = useState(1)
-  const TOTAL_PAGES: number = Math.round(items.length / itemNumber)
+  const TOTAL_PAGES: number = Math.ceil(items.length / itemNumber)
   const filterCategories = (curcat?: string) => {
     const newItems = cards.filter(newVal => {
       return newVal.category === curcat
@@ -37,18 +35,20 @@ const BlogMain: FC<PropsBlog> = props => {
     // Handle page change logic here
     setCurrentPage(pageNumber)
   }
-
+  console.log(currentPage)
   const handleShowMore = () => {
     // ...
-    if (currentPage < TOTAL_PAGES) setCurrentPage(currentPage + 1)
-    setItemNumber(9)
+    if (currentPage < TOTAL_PAGES) {
+      setCurrentPage(currentPage + 1)
+      setItemNumber(9)
+    }
   }
 
   return (
     <WrapperForMainContent>
       <div className={styles.blog}>
-        <Heading className={styles.blog__title}>Блог</Heading>
-        <Subheading className={styles.blog__path}>Главная/Блог</Subheading>
+        <h2 className={styles.blog__title}>Блог</h2>
+        <p className={styles.blog__path}>Главная/Блог</p>
       </div>
       <div className={styles.blog__wrapper}>
         <div className={styles.blog__filters}>
@@ -58,7 +58,7 @@ const BlogMain: FC<PropsBlog> = props => {
         <section className={styles.wrapper}>
           <BlogMainItem />
           <ul>
-            {items.slice(0, itemNumber).map(item => (
+            {items.slice(currentPage == 1 ? 0 : itemNumber * (currentPage - 1), itemNumber * currentPage).map(item => (
               <BlogItemForContainer key={item.id} card={item} />
             ))}
           </ul>
