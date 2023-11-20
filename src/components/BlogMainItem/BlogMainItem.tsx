fc5d219865281744ab557fc1af4598c933fd5e6c
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-11-20 07:30:14 +0300

    bugfix-blog-model

--- src/components/BlogMainItem/BlogMainItem.tsx
+++ src/components/BlogMainItem/BlogMainItem.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import styles from './blog-main-item.module.scss'
 import { blogMainItemData } from '../../mockData/blogMainItemData'
 import { useEffect, useMemo } from 'react'
-import eye from '../../assets/images/blogMainItem/icon-views.svg'
-import comment from '../../assets/images/blogMainItem/icon-comments.svg'
-import dot from '../../assets/images/blogMainItem/icon-dot.svg'
 import { fromSS } from '../../constants/constants'
+import IconComment from '../../assets/images/blogMainItem/icon-comments.svg'
+import IconEye from '../../assets/images/blogMainItem/icon-views.svg'
+import IconDot from '../../assets/images/blogMainItem/icon-dot.svg'
+import classNames from 'classnames'
 
 function BlogMainItem() {
   const mainItem = blogMainItemData
@@ -39,13 +40,13 @@ function BlogMainItem() {
       <h3>{mainItem.title || ''}</h3>
       <div className={styles.main__info}>
         <p className={styles.main__icon}>
-          <img src={eye} alt="views" draggable="false" className={styles.main__image} /> {fromSS}
+          <IconEye className={classNames(styles.main__image)} /> {fromSS}
         </p>
         <p className={styles.main__icon}>
-          <img src={comment} alt="comments" draggable="false" className={styles.main__image} />
+          <IconComment className={classNames(styles.main__image)} />
           {mainItem.comments.length}
         </p>
-        <img src={dot} alt="dot" draggable="false" className={styles.main__icon} />
+        <IconDot className={classNames(styles.main__icon)} />
         <span>{mainItem.date || ''}</span>
       </div>
     </a>
