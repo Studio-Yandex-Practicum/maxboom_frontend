546952921370dca938875a0a788973381a6d7e34
Kirill Kurentsov <immortalsteel@gmail.com>
2023-11-30 14:59:16 +0300

    fix: fix code review issues

--- src/components/HeaderAccount/HeaderAccount.tsx
+++ src/components/HeaderAccount/HeaderAccount.tsx
@@ -3,6 +3,7 @@ import PersonIcon from '@/assets/images/headerAccount/person.svg'
 import ScalesIcon from '@/assets/images/headerAccount/scales.svg'
 import HeartIcon from '@/assets/images/headerAccount/heart.svg'
 import CartIcon from '@/assets/images/headerAccount/cart.svg'
+import Popup from '@/ui/Popup/Popup'
 import styles from './headerAccount.module.scss'
 
 export type HeaderAccountProps = {
@@ -10,8 +11,7 @@ export type HeaderAccountProps = {
   total: string
 }
 
-const LazyPopup = lazy(() => import('@/ui/Popup/Popup'))
-const LazyLoginForm = lazy(() => import('@/features/login/LoginForm'))
+const LazyLoginForm = lazy(() => import('@/features/login/index'))
 
 /**
  * @param {string} counter - счетчик количества товаров в корзине
@@ -32,29 +32,29 @@ const HeaderAccount: FC<HeaderAccountProps> = ({ counter, total }) => {
     <>
       {isPopupOpen && (
         <Suspense fallback={<>Загрузка...</>}>
-          <LazyPopup isPopupOpen={isPopupOpen} onClose={changePopupState}>
+          <Popup isPopupOpen={isPopupOpen} onClose={changePopupState}>
             <LazyLoginForm />
-          </LazyPopup>
+          </Popup>
         </Suspense>
       )}
-      <div className={`${styles['header__cart-wrapper']}`}>
-        <article className={`${styles.header__cart}`}>
-          <PersonIcon className={`${styles['header__cart-image']}`} onClick={handlePersonIconClick} />
+      <div className={styles['header__cart-wrapper']}>
+        <article className={styles.header__cart}>
+          <PersonIcon className={styles['header__cart-image']} onClick={handlePersonIconClick} />
         </article>
 
-        <article className={`${styles.header__cart}`}>
+        <article className={styles.header__cart}>
           <ScalesIcon className={styles['header__cart-image']} />
-          <div className={`${styles.header__line}`}></div>
+          <div className={styles.header__line}></div>
           <HeartIcon className={styles['header__cart-image']} />
         </article>
-        <article className={`${styles.header__cart}`}>
+        <article className={styles.header__cart}>
           <CartIcon className={styles['header__cart-image']} />
-          <div className={`${styles['header__cart-container']}`}>
-            <div className={`${styles['header__counter-container']}`}>
-              <p className={`${styles['header__cart-total-text']}`}>Корзина</p>
-              <p className={`${styles['header__cart-counter']}`}>{counter}</p>
+          <div className={styles['header__cart-container']}>
+            <div className={styles['header__counter-container']}>
+              <p className={styles['header__cart-total-text']}>Корзина</p>
+              <p className={styles['header__cart-counter']}>{counter}</p>
             </div>
-            <p className={`${styles['header__cart-total']}`}>{total}</p>
+            <p className={styles['header__cart-total']}>{total}</p>
           </div>
         </article>
       </div>
