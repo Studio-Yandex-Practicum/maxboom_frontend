f320e89add817d03da33bc887ab686f6fbb18b37
Kirill Kurentsov <immortalsteel@gmail.com>
2023-12-03 11:01:49 +0300

    feat: Форма авторизации пользователя (#108)
    * feat: Форма авторизации пользователя #99
    
    * fix: fix code review issues
    
    * refactor: Добавил тайм-аут на сабмит формы

---
+++ src/features/login/model/validation/validation.ts
@@ -0,0 +1,9 @@
+import * as Yup from 'yup'
+
+export const validationSchema = Yup.object().shape({
+  email: Yup.string().required('Введите электронную почту').email('Укажите корректный адрес электронной почты'),
+  password: Yup.string()
+    .required('Введите пароль')
+    .min(8, 'Минимальная длина пароля 8 символов')
+    .max(64, 'Максимальная длина пароля 64 символа')
+})
--- src/ui/Popup/Popup.tsx
+++ src/ui/Popup/Popup.tsx
@@ -10,6 +10,7 @@ interface IPopupProps extends HTMLAttributes<HTMLElement> {
   className?: string | undefined
 }
 
+// Поменял импорт на дефолтный, чтобы можно было использовать React.lazy
 // @TODO: Ограничить перемещение табом внутри одного поп-апа
 // https://github.com/Studio-Yandex-Practicum/maxboom_frontend/issues/106
 
@@ -20,7 +21,7 @@ interface IPopupProps extends HTMLAttributes<HTMLElement> {
  * @param {function} onClose - handler function to close the popup.
  * @param {string} className - styles passed from the parent component.
  */
-export const Popup = ({ isPopupOpen, onClose, className, children }: IPopupProps) => {
+export default function Popup({ isPopupOpen, onClose, className, children }: IPopupProps) {
   const popupRef = useRef<HTMLDivElement>(null)
   const [isPopupClosing, setIsPopupClosing] = useState(false)
 
