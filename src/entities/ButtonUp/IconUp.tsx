eb00ab99b65d9a6cceddda9ac68b3a98967a198a
Dmitrii Gudemenko <dmitry.gudemenko@yandex.ru>
2023-12-21 18:52:57 +0300

    fixed after review

--- src/entities/ButtonUp/IconUp.tsx
+++ src/entities/ButtonUp/IconUp.tsx
@@ -1,15 +1,26 @@
 import styles from './ButtonUp.module.scss'
 import IconArrow from '@/assets/icons/IconArrow'
 import { FC } from 'react'
-
+import { Button, ButtonSize, ButtonTheme } from '@/shared/ui/Button/Button'
 interface IconUpProps {
   className?: string
-  onClick?: FC
+  goToTop?: React.MouseEventHandler<HTMLButtonElement>
 }
-const IconUp: FC<IconUpProps> = () => {
+/*
+ * Компонент кнопки Вверх
+ */
+const IconUp: FC<IconUpProps> = props => {
+  const { goToTop } = props
   return (
-    <div className={styles.topToBtm}>
-      <IconArrow styles={styles.iconPosition} type="next" />
+    <div className={styles.top__tobtm}>
+      <Button
+        onClick={goToTop}
+        size={ButtonSize.S}
+        theme={ButtonTheme.PRIMARY}
+        className={styles.icon__position}
+        type="submit">
+        <IconArrow styles={styles.icon__position} type="next" />
+      </Button>
     </div>
   )
 }
--- src/features/ScrollUp/ScrollUp.tsx
+++ src/features/ScrollUp/ScrollUp.tsx
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react'
 import styles from './ScrollUp.module.scss'
 import classNames from 'classnames'
 import IconUp from '@/entities/ButtonUp/IconUp'
+
+/*
+ * Кнопка Вверх для быстрого перемещения в начало страницы
+ */
 const ScrollToTop = () => {
   const [showTopBtn, setShowTopBtn] = useState(false)
   useEffect(() => {
@@ -13,13 +17,14 @@ const ScrollToTop = () => {
       }
     })
   }, [])
+
   const goToTop = () => {
-    console.log('1')
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
     })
   }
-  return <div onClick={goToTop}> {showTopBtn && <IconUp className={classNames(styles.iconPosition)} />} </div>
+
+  return <div> {showTopBtn && <IconUp goToTop={goToTop} className={classNames(styles.iconPosition)} />} </div>
 }
 export default ScrollToTop
