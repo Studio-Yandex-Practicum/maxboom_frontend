8bd968be628f067911bf0c45171d0b5741dc7a2f
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-12-26 00:05:11 +0300

    переделать компонент footer под FSD

---
+++ src/mockData/coreBaseData.ts
@@ -0,0 +1,54 @@
+export const coreBaseData = {
+  header: {
+    main_logo: {
+      image: 'https://maxboom.ru/image/catalog/Logo/IMG_5735.JPG',
+      url: '/',
+      title: 'Интернет-магазин «Maxboom.ru»'
+    },
+    support: {
+      name: 'Обрантый звонок',
+      phone_number: '+7 977 848-02-28'
+    }
+  },
+  footer: {
+    company_info: 'Интернет-магазин «Maxboom.ru»',
+    disclaimer: 'Мы не будем присылать вам спам. Только скидки и выгодные предложения',
+    support_work_time: 'Будни, с 10.00 до 20.00',
+    main_logo: {
+      image: 'https://maxboom.ru/image/catalog/Logo/IMG_5735.JPG',
+      url: '/',
+      title: 'Интернет-магазин «Maxboom.ru»'
+    },
+    additional_logos: [
+      {
+        image: 'https://maxboom.ru/image/catalog/demo-prostore/svg-icon/icon-payments-visa.svg',
+        url: '//visa.ru',
+        title: 'visa'
+      },
+      {
+        image: 'https://maxboom.ru/image/catalog/demo-prostore/svg-icon/icon-payments-mastercard.svg',
+        url: '//mastercard.ru',
+        title: 'mastercard'
+      },
+      {
+        image: 'https://maxboom.ru/image/catalog/demo-prostore/svg-icon/mir-logo-h14px.svg',
+        url: '//mironline.ru',
+        title: 'мир'
+      },
+      {
+        image: 'https://maxboom.ru/image/catalog/demo-prostore/svg-icon/icons8-webmoney-25.svg',
+        url: '//webmoney.ru',
+        title: 'webmoney'
+      },
+      {
+        image: 'https://maxboom.ru/image/catalog/demo-prostore/svg-icon/io.svg',
+        url: '//yoomoney.ru',
+        title: 'yoomoney'
+      }
+    ],
+    support: {
+      name: 'Обрантый звонок',
+      phone_number: '+7 977 848-02-28'
+    }
+  }
+}
