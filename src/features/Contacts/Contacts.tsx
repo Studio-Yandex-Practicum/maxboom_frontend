03c96874fc9e84eae1c819edc8f06dd1e21629dc
Artem Sibisov <artsib7777@yandex.ru>
2024-01-08 18:57:44 +0800

    #176 bugfix: небольшая корректировка установки стилей для меню контактов с применением classnames

--- src/features/Contacts/Contacts.tsx
+++ src/features/Contacts/Contacts.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react'
+import classNames from 'classnames'
 import { TMessenger } from '@/models/MessengerModel'
 import styles from './contacts.module.scss'
 import ContactCard from '@/entities/ContactCard/ContactCard'
@@ -24,10 +25,7 @@ const Contacts: FC<PropsContacts> = ({ messenger }) => {
 
   return (
     <div className={styles.contacts}>
-      <div
-        className={
-          messageMenuActive ? styles.contactsMenu : `${styles.contactsMenu} ${styles.contactsMenuHidden}`
-        }>
+      <div className={classNames(styles.contactsMenu, { [styles.contactsMenuHidden]: !messageMenuActive })}>
         <ul className={styles.contactList}>
           {messenger.map(item => (
             <ContactCard messenger={item} key={item.id} Icon={item.icon} />
