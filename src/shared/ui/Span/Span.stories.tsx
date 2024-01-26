cd06841899c6a1878e089d0a2be3655cb3274aa2
Artur Khelshtein <Khelshtein.a@yandex.ru>
2024-01-26 16:14:19 +0300

    сделать Span и Label компонентами

---
+++ src/shared/ui/Span/Span.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import Span from './Span'
+
+const meta = {
+  title: 'shared/Span',
+  component: Span,
+  parameters: {
+    layout: 'centered'
+  },
+  tags: ['autodocs']
+} satisfies Meta<typeof Span>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    children: '*'
+  }
+}
