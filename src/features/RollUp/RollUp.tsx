bfdb2eff6a589491f65b69bc74c893aeeeb8e6fb
kirill-k88 <kirill-k88@ya.ru>
2024-02-26 15:14:01 +0300

    fix: актуализировал компонент RollUp в соответствии с задачей #216

--- src/features/RollUp/RollUp.tsx
+++ src/features/RollUp/RollUp.tsx
@@ -1,4 +1,4 @@
-import { useState, type FC } from 'react'
+import { useState, type FC, PropsWithChildren } from 'react'
 
 import IconArrowDown from '@/assets/icons/IconArrowDown.svg'
 import IconArrowUp from '@/assets/icons/IconArrowUp.svg'
@@ -11,7 +11,7 @@ import styles from './RollUp.module.scss'
  * Компонент разворачивающегося блока
  * @param {children} React.ReactNode - контент;
  */
-export const RollUp: FC<{ children: React.ReactNode }> = ({ children }) => {
+export const RollUp: FC<PropsWithChildren> = ({ children }) => {
   const [isRolledOut, setIsRolledOut] = useState(false)
 
   const onButtonClickHandl = () => {
@@ -20,15 +20,22 @@ export const RollUp: FC<{ children: React.ReactNode }> = ({ children }) => {
 
   return (
     <section className={styles.rollup}>
-      <div className={styles.rollup__header}>
-        <Heading type={HeadingType.NORMAL}>Описание</Heading>
+      <div
+        className={`${styles.rollup__header} ${!isRolledOut && styles.rollup__header_rolledup}`}
+        onClick={onButtonClickHandl}>
+        <Heading className={styles.rollup__headertext} type={HeadingType.NORMAL}>
+          Описание
+        </Heading>
         <Button
           design={ButtonDesign.SQUARE}
           size={ButtonSize.M}
           type="button"
-          className={styles.rollup__button}
-          onClick={onButtonClickHandl}>
-          {isRolledOut ? <IconArrowUp /> : <IconArrowDown />}
+          className={styles.rollup__button}>
+          {isRolledOut ? (
+            <IconArrowUp className={styles.rollup__buttonicon} />
+          ) : (
+            <IconArrowDown className={styles.rollup__buttonicon} />
+          )}
         </Button>
       </div>
       <div className={`${styles.rollup__rollupchild} ${!isRolledOut && styles.rollup__rollupchild_rolledup}`}>
