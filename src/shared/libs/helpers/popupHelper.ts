a6a09ad33b7e8b8a93d54ffe33708b8b5fcd7982
kirill-k88 <kirill-k88@ya.ru>
2024-03-30 20:39:07 +0300

    Адаптировал под различные разрешения и высоту. Добавил попап с формой.

--- src/shared/libs/helpers/popupHelper.ts
+++ src/shared/libs/helpers/popupHelper.ts
@@ -1,7 +1,8 @@
 /**
  * Функция скрывает или отображает вертикальную прокрутку у body.
  * Используется при открытии попапа
- * @param [needsToHide: boolean] - true - ф-я скроект прокрутку, false - вернет как было до скрытия (значение по умполчанию - false).
+ *
+ * @param {boolean} needsToHide если true, то ф-я скроект прокрутку, false - вернет как было до скрытия (значение по умполчанию - false).
  */
 export const bodyScrollControl = (needsToHide: boolean = false): void => {
   if (needsToHide) {
