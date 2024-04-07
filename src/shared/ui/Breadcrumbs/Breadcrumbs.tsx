8c4d34ba9dd254fd874289b53a07378a411f5fe5
Yulia Avramenko <miraclejuliet@yandex.ru>
2024-04-07 23:23:32 +0300

    #293-enhancement-create-Breadcrumbs-Component

---
+++ src/shared/ui/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,42 @@
+import { FC } from 'react'
+
+import Link from '../Link/Link'
+import Subheading from '../Subheading/Subheading'
+
+import styles from './Breadcrumbs.module.scss'
+
+export type TLinkProps = {
+  heading: string
+  href: string
+}
+type TBreadcrumbsProps = {
+  links: TLinkProps[]
+}
+
+/**
+ * shared/ui компонент хлебные крошки, принимает список ссылок и рендерит их;
+ * @param {TLinkProps[]} links - ссылки, которые попадают в хлебные крошки
+ * @param {string} heading - название категории
+ * @param {string} href - ссылка
+ */
+
+const Breadcrumbs: FC<TBreadcrumbsProps> = ({ links }) => {
+  return (
+    <div className={styles.breadcrumbs}>
+      {links.map((link, index) => {
+        return (
+          (index !== links.length - 1 && (
+            <Subheading>
+              <Link key={link.href} to={link.href} className={styles.link}>
+                {link.heading}{' '}
+              </Link>
+              <span>{` / `}</span>
+            </Subheading>
+          )) || <Subheading key={link.href}>{link.heading}</Subheading>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Breadcrumbs
