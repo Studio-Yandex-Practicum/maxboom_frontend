5c5a55a153bdb62844bea10e514bfee95062744e
kirill-k88 <kirill-k88@ya.ru>
2024-04-19 12:53:00 +0300

    Добавил breadcrumbs на страницу товара. Немного поправил компонент breadcrumbs в соответствии с сайтом MaxBoom

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -4,8 +4,9 @@ import { useParams } from 'react-router'
 
 import { StateSchema } from '@/app/providers/StoreProvider'
 import { AppDispatch } from '@/app/providers/StoreProvider/config/store'
-import { PageDescription } from '@/components/PageDescription/PageDescription'
 import WrapperForMainContent from '@/components/WrapperForMainContent/WrapperForMainContent'
+import Breadcrumbs from '@/shared/ui/Breadcrumbs/Breadcrumbs'
+import Heading from '@/shared/ui/Heading/Heading'
 import Advantages from '@/widgets/Advantages/ui/Advantages/Advantages'
 import { Product } from '@/widgets/Product/Product'
 import { ProductInfo } from '@/widgets/ProductInfo/ProductInfo'
@@ -13,6 +14,7 @@ import { ViewedProducts } from '@/widgets/ViewedProducts/ViewedProducts'
 
 import { addToViewedProducts } from './model/functions/functions'
 import { getProduct } from './model/slice/productSlice'
+import styles from './ProductPage.module.scss'
 
 /**
  * Страница с выбранным товаром.
@@ -22,6 +24,11 @@ export const ProductPage = () => {
   const dispatch = useDispatch<AppDispatch>()
   const productStore = useSelector((store: StateSchema) => store.product)
   const { slug } = useParams()
+
+  const links = [
+    { heading: 'Главная', href: '/' },
+    { heading: productStore.product.name, href: '' }
+  ]
 
   useEffect(() => {
     if (slug) dispatch(getProduct(slug))
@@ -34,7 +41,10 @@ export const ProductPage = () => {
   return (
     <>
       <WrapperForMainContent>
-        <PageDescription count={-1} heading={productStore.product.name} />
+        <div className={styles.pageDescriptor}>
+          <Heading>{productStore.product.name}</Heading>
+          <Breadcrumbs links={links} />
+        </div>
         <Product product={productStore.product} />
         <ProductInfo description={productStore.product.description} />
         <Advantages />
