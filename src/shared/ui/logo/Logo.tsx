b159e5574e041c57ddadd3575c6806b2e313c798
Alexander Morugin <nobilis@bk.ru>
2024-04-26 17:38:11 +0300

    enhancement_306_header_logo_skeleton

--- src/shared/ui/logo/Logo.tsx
+++ src/shared/ui/logo/Logo.tsx
@@ -4,12 +4,8 @@ import { Routes } from '@/shared/config/routerConfig/routes'
 import Link from '@/shared/ui/Link/Link'
 
 import styles from './logo.module.scss'
+import { TLogoProps } from './model/types/types'
 
-type TLogoProps = {
-  image: string
-  width: string
-  height: string
-}
 /**
  * @param {string} image - путь к логотипу на сервере
  * @param {string} width - ширина логотипа
@@ -18,11 +14,9 @@ type TLogoProps = {
 
 const Logo: FC<TLogoProps> = ({ image, width, height }) => {
   return (
-    <div className={styles.container}>
-      <Link to={Routes.HOME} className={styles.link}>
-        <img src={image} alt="Logo" style={{ width, height }} />
-      </Link>
-    </div>
+    <Link to={Routes.HOME} className={styles.link}>
+      <img src={image} alt="Logo" style={{ width, height }} />
+    </Link>
   )
 }
 
