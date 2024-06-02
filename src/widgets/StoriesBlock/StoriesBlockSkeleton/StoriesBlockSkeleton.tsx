044bd3c1a9fe21cc54f1c1bfa78535c40d260476
Kseniya <ksuta_marfa@mil.ru>
2024-06-02 20:46:07 +0300

    skeleton-StoriesBlock

---
+++ src/widgets/StoriesBlock/StoriesBlockSkeleton/StoriesBlockSkeleton.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react'
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+import styles from './StoriesBlockSkeleton.module.scss'
+
+const StoriesBlockSkeleton: FC = () => {
+  return (
+    <div className={styles.skeletonNewsCard}>
+      <Skeleton height={180} />
+      <div className={styles.content}>
+        <Skeleton width={`60%`} />
+        <Skeleton width={`80%`} />
+      </div>
+    </div>
+  )
+}
+
+export default StoriesBlockSkeleton
